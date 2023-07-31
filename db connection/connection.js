10c457876f485a1b7430c82999a2ab30b896f85d
shubham <shubham.email01@gmail.com>
2023-07-31 13:45:02 +0530

    conn str changed

--- db connection/connection.js
+++ db connection/connection.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
-
 const CONN_STRING = process.env.CONN_STR;
-
-console.log(process.env.CONN_STR)
 
 module.exports = mongoose.connect(CONN_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log("connection established with mongodb server online"); })
